7e9e703b72dff307131c1886953e7b5c9eda348a
admincurriculum68 <admin@curriculum68.go.th>
2026-07-16 16:23:22 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./dashboard.html", "./js/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
